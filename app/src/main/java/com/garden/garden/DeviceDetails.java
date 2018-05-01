b6b9affeb032d7ebb9c5b2bf876a6e2cbe310af0
Adithya <adithyadinesh96@gmail.com>
2018-05-01 10:26:13 +0530

    Device Name Change and Device Mode Feature
    Adding an option to change device Name.
    Adding modes to Device.
    Restricting changing switch if its in automatic mode.

--- app/src/main/java/com/garden/garden/DeviceDetails.java
+++ app/src/main/java/com/garden/garden/DeviceDetails.java
@@ -9,6 +9,8 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.text.InputType;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -17,6 +19,8 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.ToggleButton;
 
+import com.afollestad.materialdialogs.DialogAction;
+import com.afollestad.materialdialogs.MaterialDialog;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -43,6 +47,12 @@ public class DeviceDetails extends AppCompatActivity implements CompoundButton.O
     private TextView nav_name;
     private TextView nav_email;
     private ImageView changeDeviceName;
+    private String deviceNameText;
+    private MaterialDialog deviceNameDialog;
+    private ToggleButton deviceMode;
+    private int deviceModeValue;
+    private MaterialDialog alert_dialog;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -57,6 +67,8 @@ public class DeviceDetails extends AppCompatActivity implements CompoundButton.O
         changeDeviceName = findViewById(R.id.changeDeviceName);
         changeDeviceName.setOnClickListener(this);
         waterSwitch.setOnCheckedChangeListener(this);
+        deviceMode = findViewById(R.id.device_mode_toggle);
+        deviceMode.setOnCheckedChangeListener(this);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
@@ -66,7 +78,7 @@ public class DeviceDetails extends AppCompatActivity implements CompoundButton.O
         toggle.syncState();
         NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
-        View header=navigationView.getHeaderView(0);
+        View header = navigationView.getHeaderView(0);
         nav_name = header.findViewById(R.id.nav_name);
         nav_email = header.findViewById(R.id.nav_email);
         Bundle extras = getIntent().getExtras();
@@ -87,7 +99,8 @@ public class DeviceDetails extends AppCompatActivity implements CompoundButton.O
                 @Override
                 public void onDataChange(DataSnapshot dataSnapshot) {
                     Picasso.get().load(dataSnapshot.child("image_url").getValue().toString()).placeholder(R.drawable.loading).into(deviceImage);
-                    deviceName.setText(dataSnapshot.child("device_name").getValue().toString());
+                    deviceNameText = dataSnapshot.child("device_name").getValue().toString();
+                    deviceName.setText(deviceNameText);
                     plantName.setText(dataSnapshot.child("plant_name").getValue().toString());
                     switchState = Integer.parseInt(dataSnapshot.child("switch_state").getValue().toString());
                     if (switchState == 1) {
@@ -96,13 +109,19 @@ public class DeviceDetails extends AppCompatActivity implements CompoundButton.O
                         waterState.setText("Off");
                     }
                     temperature.setText(dataSnapshot.child("temperature").getValue().toString());
-                    if(Integer.parseInt(dataSnapshot.child("switch_state").getValue().toString()) == 1){
+                    if (Integer.parseInt(dataSnapshot.child("switch_state").getValue().toString()) == 1) {
                         waterSwitch.setChecked(true);
-                    }
-                    else {
+                    } else {
                         waterSwitch.setChecked(false);
                     }
+                    deviceModeValue = Integer.parseInt(dataSnapshot.child("mode").getValue().toString());
+                    if (deviceModeValue == 1) {
+                        deviceMode.setChecked(true);
+                    } else {
+                        deviceMode.setChecked(false);
+                    }
                 }
+
                 @Override
                 public void onCancelled(DatabaseError databaseError) {
 
@@ -110,7 +129,8 @@ public class DeviceDetails extends AppCompatActivity implements CompoundButton.O
             });
         }
     }
-    private void gotoLogin(){
+
+    private void gotoLogin() {
         Intent in = new Intent(this, LoginActivity.class);
         in.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
         startActivity(in);
@@ -118,11 +138,40 @@ public class DeviceDetails extends AppCompatActivity implements CompoundButton.O
 
     @Override
     public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-        if(isChecked){
-            device_ref.child("switch_state").setValue(1);
+        if (buttonView == waterSwitch) {
+            if (deviceModeValue == 1) {
+                if (switchState == 1) {
+                    waterSwitch.setChecked(true);
+                } else {
+                    waterSwitch.setChecked(false);
+                }
+                alert_dialog = new MaterialDialog.Builder(this)
+                        .content(R.string.automatic_mode)
+                        .positiveText("Ok")
+                        .onPositive(new MaterialDialog.SingleButtonCallback() {
+                            @Override
+                            public void onClick(@NonNull MaterialDialog dialog, @NonNull DialogAction which) {
+                                alert_dialog.dismiss();
+                            }
+                        })
+                        .cancelable(false)
+                        .show();
+
+            } else {
+                if (isChecked) {
+                    device_ref.child("switch_state").setValue(1);
+                } else {
+                    device_ref.child("switch_state").setValue(0);
+                }
+            }
+
         }
-        else {
-            device_ref.child("switch_state").setValue(0);
+        if (buttonView == deviceMode) {
+            if (isChecked) {
+                device_ref.child("mode").setValue(1);
+            } else {
+                device_ref.child("mode").setValue(2);
+            }
         }
     }
 
@@ -157,9 +206,10 @@ public class DeviceDetails extends AppCompatActivity implements CompoundButton.O
     }
 
     private void goMarket() {
-        Intent in = new Intent(this,MarketActivity.class);
+        Intent in = new Intent(this, MarketActivity.class);
         startActivity(in);
     }
+
     private void gotoHome() {
         Intent in = new Intent(this, MainActivity.class);
         in.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
@@ -170,7 +220,7 @@ public class DeviceDetails extends AppCompatActivity implements CompoundButton.O
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
-        if(id == R.id.nav_my_devices){
+        if (id == R.id.nav_my_devices) {
             gotoHome();
         }
         if (id == R.id.nav_logout) {
@@ -178,7 +228,7 @@ public class DeviceDetails extends AppCompatActivity implements CompoundButton.O
             Checkout.clearUserData(this);
             gotoLogin();
         }
-        if(id == R.id.nav_market){
+        if (id == R.id.nav_market) {
             goMarket();
         }
 
@@ -189,8 +239,36 @@ public class DeviceDetails extends AppCompatActivity implements CompoundButton.O
 
     @Override
     public void onClick(View v) {
-        if (v == changeDeviceName){
-            
+        if (v == changeDeviceName) {
+            deviceNameDialog = new MaterialDialog.Builder(this)
+                    .title(R.string.enter_device_name)
+                    .content(R.string.device_name_content)
+                    .inputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_CLASS_TEXT)
+                    .alwaysCallInputCallback()
+                    .input(" ", deviceNameText, new MaterialDialog.InputCallback() {
+
+                                @Override
+                                public void onInput(@NonNull MaterialDialog dialog, CharSequence input) {
+                                    deviceNameText = input.toString();
+                                }
+                            }
+                    )
+                    .positiveText(R.string.save)
+                    .negativeText(R.string.cancel)
+                    .onPositive(new MaterialDialog.SingleButtonCallback() {
+                        @Override
+                        public void onClick(@NonNull MaterialDialog dialog, @NonNull DialogAction which) {
+                            device_ref.child("device_name").setValue(deviceNameText);
+                            deviceNameDialog.dismiss();
+                        }
+                    })
+                    .onNegative(new MaterialDialog.SingleButtonCallback() {
+                        @Override
+                        public void onClick(@NonNull MaterialDialog dialog, @NonNull DialogAction which) {
+                            deviceNameDialog.dismiss();
+                        }
+                    })
+                    .show();
         }
     }
 }
